10483ab03ad1879f63d4f1da61776d8044ce15f0
bhumirajbhar <146117332+thepbhumirajbhar@users.noreply.github.com>
2026-08-14 01:02:57 +0530

    Time: 6 ms (89.37%) | Memory: 43.8 MB (74.39%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,26 +1,49 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        Map<Character, Character> m1 = new HashMap<>();
-        Map<Character, Character> m2 = new HashMap<>();
+        // APPROACH 2
 
         if(s.length() != t.length()) return false;
 
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+
         for(int i = 0; i < s.length(); i++){
-            if(m1.containsKey(s.charAt(i))){
-                if(m1.get(s.charAt(i)) != t.charAt(i)) return false;
-            }
-            else{
-                m1.put(s.charAt(i), t.charAt(i));
-            } 
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
 
+            // Agar last seen positions match nahi karti, toh mapping breakdown ho chuki hai
+            if(m1[c1] != m2[c2]) return false;
 
-            if(m2.containsKey(t.charAt(i))){
-                if(m2.get(t.charAt(i)) != s.charAt(i)) return false;
-            }
-            else{
-                m2.put(t.charAt(i), s.charAt(i));
-            }
+            m1[c1] = i+1;
+            m2[c2] = i+1;
         }
         return true;
+
+
+
+
+    // APPROACH 1    
+        // Map<Character, Character> m1 = new HashMap<>();
+        // Map<Character, Character> m2 = new HashMap<>();
+
+        // if(s.length() != t.length()) return false;
+
+        // for(int i = 0; i < s.length(); i++){
+        //     if(m1.containsKey(s.charAt(i))){
+        //         if(m1.get(s.charAt(i)) != t.charAt(i)) return false;
+        //     }
+        //     else{
+        //         m1.put(s.charAt(i), t.charAt(i));
+        //     } 
+
+
+        //     if(m2.containsKey(t.charAt(i))){
+        //         if(m2.get(t.charAt(i)) != s.charAt(i)) return false;
+        //     }
+        //     else{
+        //         m2.put(t.charAt(i), s.charAt(i));
+        //     }
+        // }
+        // return true;
     }
 }
