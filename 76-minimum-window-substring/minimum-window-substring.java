0d1810f8e0c193e788dbff43640ccc30e426bf75
bhumirajbhar <146117332+thepbhumirajbhar@users.noreply.github.com>
2026-08-20 01:22:56 +0530

    Time: 2 ms (99.80%) | Memory: 45.3 MB (89.20%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,36 +1,35 @@
 class Solution {
     public String minWindow(String s, String t) {
         int m = s.length(), n = t.length();
-        int[] need = new int[128];       // frequency of each char needed from t
-        int required = 0;               // total unique chars in t that need to be satisfied
+        if(m < n) return "";
 
-        for( int i = 0; i < n; i++){
-            need[t.charAt(i)]++;
-            if(need[t.charAt(i)] == 1) required++;
+        int[] arr = new int[128];
+        for(char c: t.toCharArray()){
+            arr[c]++;
         }
 
-        int[] have = new int[128];        // frequency of each char in current window
-        int formed = 0;                  // how many unique chars from t are fully satisfied
-        int left = 0;
-        int minLen = Integer.MAX_VALUE;
-        int ansLeft = 0, ansRight = 0;
+        int count = n;       // Characters remaining to match
+        int left = 0, minLen = Integer.MAX_VALUE, start = 0;
 
         for(int right = 0; right < m; right++){
-            char ch = s.charAt(right);
-            have[ch]++;
-            if(have[ch] == need[ch]) formed++;
-            while(formed == required){
-                if(right - left + 1 < minLen){
+            char rightChar = s.charAt(right);
+            if(arr[rightChar] > 0){
+                count--;
+            }
+            arr[rightChar]--;
+
+            while(count == 0){
+                if (right - left + 1 < minLen) {
                     minLen = right - left + 1;
-                    ansLeft = left;
-                    ansRight = right;
-                }  
-                char leftChar = s.charAt(left);
-                have[leftChar]--;              
-                if(have[leftChar] < need[leftChar]) formed--;
-                left++;
+                    start = left;
+                }
+            
+            char leftChar = s.charAt(left);
+            arr[leftChar]++;
+            if (arr[leftChar] > 0) count++;
+            left++;
             }
         }
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(ansLeft, ansRight + 1);
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 }
