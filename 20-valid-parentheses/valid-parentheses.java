72c7d32a9fa19e1894748d928d8b7e57465136bd
bhumirajbhar <146117332+thepbhumirajbhar@users.noreply.github.com>
2026-08-21 16:31:55 +0530

    Time: 1 ms (99.93%) | Memory: 43.1 MB (85.95%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,23 +1,51 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(int i = 0; i<s.length(); i++){
-            char ch = s.charAt(i);
-            if (ch == '(' || ch == '[' || ch == '{') st.push(ch);
+    // Method 2: 
+       if(s.length() % 2 != 0) return false;
 
-            else if(ch == ')') {
-                if(st.isEmpty() || st.peek() != '(') return false;
-                else st.pop();
-            }
-            else if(ch == ']') {
-                if(st.isEmpty() || st.peek() != '[') return false;
-                else st.pop();
-            }
-            else if(ch == '}') {
-                if(st.isEmpty() || st.peek() != '{') return false;
-                else st.pop();
-            }
+       char[] stack  = new char[s.length()];
+       int head = 0;
+
+       for(char ch: s.toCharArray()){
+        if(ch == '('){
+            stack[head++] = ')';
         }
-        return st.isEmpty();
+        else if(ch == '{'){
+            stack[head++] = '}';
+        }
+        else if(ch == '['){
+            stack[head++] = ']';
+        }
+        else if(head == 0 || stack[--head] != ch){
+            return false;
+        }
+       }  
+       return head == 0; 
+
+
+
+
+
+
+    // Method 1:     
+        // Stack<Character> st = new Stack<>();
+        // for(int i = 0; i<s.length(); i++){
+        //     char ch = s.charAt(i);
+        //     if (ch == '(' || ch == '[' || ch == '{') st.push(ch);
+
+        //     else if(ch == ')') {
+        //         if(st.isEmpty() || st.peek() != '(') return false;
+        //         else st.pop();
+        //     }
+        //     else if(ch == ']') {
+        //         if(st.isEmpty() || st.peek() != '[') return false;
+        //         else st.pop();
+        //     }
+        //     else if(ch == '}') {
+        //         if(st.isEmpty() || st.peek() != '{') return false;
+        //         else st.pop();
+        //     }
+        // }
+        // return st.isEmpty();
     }
 }
