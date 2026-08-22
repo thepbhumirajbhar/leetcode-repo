d9af991d50be71de8a8e3c485d3dd87b0c44e8ba
bhumirajbhar <146117332+thepbhumirajbhar@users.noreply.github.com>
2026-08-22 18:28:35 +0530

    Time: 7 ms (96.82%) | Memory: 47.1 MB (79.41%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -1,20 +1,62 @@
 class Solution {
     public String minRemoveToMakeValid(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        Deque<Integer> stack = new ArrayDeque<>();
+    // METHOD 2: 
+    char[] arr = s.toCharArray();
+    int open = 0;
 
-        for(int i = 0; i<s.length(); i++){
-            if(s.charAt(i) == '(') stack.push(i);
-            else if(s.charAt(i) == ')'){
-                if(!stack.isEmpty()) stack.pop();
-                else{
-                    sb.setCharAt(i,'*');
-                }
+    // Pass 1: Remove invalid ')' from left to right
+    for(int i = 0; i<arr.length; i++){
+        if(arr[i] == '('){
+            open++;
+        }
+        else if( arr[i] == ')'){
+            if(open == 0) arr[i] = '*';
+            else{
+                open--;
             }
         }
-        while(!stack.isEmpty()){
-            sb.setCharAt(stack.pop(), '*');
+    }
+
+    // Pass 2: Remove excess '(' from right to left
+    for(int i = arr.length-1; i >= 0; i--){
+        if(open > 0 && arr[i] == '('){
+            arr[i] = '*';
+            open--;
         }
-        return sb.toString().replace("*","");
+    }
+
+    // Pass 3: Build cleaned string
+    StringBuilder sb = new StringBuilder();
+    for(char c: arr){
+        if(c != '*') sb.append(c);
+    }
+
+    return sb.toString();
+
+
+
+
+
+
+
+    // METHOD 1: uses a stack to track open parentheses, marks all unmatched ( and ) with *, and then strips out those placeholders to produce a valid string.
+
+
+        // StringBuilder sb = new StringBuilder(s);
+        // Deque<Integer> stack = new ArrayDeque<>();
+
+        // for(int i = 0; i<s.length(); i++){
+        //     if(s.charAt(i) == '(') stack.push(i);
+        //     else if(s.charAt(i) == ')'){
+        //         if(!stack.isEmpty()) stack.pop();
+        //         else{
+        //             sb.setCharAt(i,'*');
+        //         }
+        //     }
+        // }
+        // while(!stack.isEmpty()){
+        //     sb.setCharAt(stack.pop(), '*');
+        // }
+        // return sb.toString().replace("*","");
     }
 }
