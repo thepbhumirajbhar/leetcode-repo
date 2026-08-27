5f36fe00326f8c1daf51ae5395c93a39b3a44f19
bhumirajbhar <146117332+thepbhumirajbhar@users.noreply.github.com>
2026-08-28 02:09:44 +0530

    Time: 1 ms (87.88%) | Memory: 45.6 MB (28.90%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,24 +1,55 @@
 class Solution {
-    public List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permute(int[] nums){
         List<List<Integer>> result = new ArrayList<>();
-        helper(nums, new ArrayList<>(nums.length), result);
+        helper(nums, new boolean[nums.length], new ArrayList<>(nums.length), result);
         return result;
     }
 
-    private void helper(int[] nums, List<Integer> current, List<List<Integer>> result){
+    private void helper(int[] nums, boolean[] visited, List<Integer> current, List<List<Integer>> result){
 
         // Base Case
-        if(current.size() == nums.length ){
-            result.add(new ArrayList<Integer> (current));
+        if(current.size() == nums.length){
+            result.add(new ArrayList<>(current));
             return;
         }
 
-        // Recurse case
-        for(int i = 0; i < nums.length; i++){
-            if(current.contains(nums[i])) continue;
+        // Recurse Case
+        for(int i = 0; i< nums.length; i++){
+            if (visited[i]) continue;
+
+            visited[i] = true;
             current.add(nums[i]);
-            helper(nums, current, result);
+
+            helper(nums, visited, current, result);
+
+            // Backtrack
             current.remove(current.size()-1);
+            visited[i] = false;
         }
     }
+
+
+
+    // public List<List<Integer>> permute(int[] nums) {
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     helper(nums, new ArrayList<>(nums.length), result);
+    //     return result;
+    // }
+
+    // private void helper(int[] nums, List<Integer> current, List<List<Integer>> result){
+
+    //     // Base Case
+    //     if(current.size() == nums.length ){
+    //         result.add(new ArrayList<Integer> (current));
+    //         return;
+    //     }
+
+    //     // Recurse case
+    //     for(int i = 0; i < nums.length; i++){
+    //         if(current.contains(nums[i])) continue;
+    //         current.add(nums[i]);
+    //         helper(nums, current, result);
+    //         current.remove(current.size()-1);
+    //     }
+    // }
 }
